37bfdee4846f2e565288d2dfcc4b2bd7a488e54f
Chibuzor Abosi <chibuzorabosi93@gmail.com>
2024-02-24 17:45:03 +0100

    Creating transactions pages

---
+++ Vehicle/Controllers/TransactionsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vehicle.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        public IActionResult InProgress()
+        {
+            return View();
+        }
+
+        public IActionResult Completed()
+        {
+            return View();
+        }
+
+        public IActionResult Returned()
+        {
+            return View();
+        }
+
+        public IActionResult Cancelled()
+        {
+            return View();
+        }
+
+        public IActionResult Deleted()
+        {
+            return View();
+        }
+    }
+}
